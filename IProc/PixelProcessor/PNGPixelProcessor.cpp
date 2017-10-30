df29b480ee2512f66db7cb3c26df8913b989ea9b
Heshan Eranga <erangamx@gmail.com>
2017-10-30 23:32:58 +0530

    added PNGPixelProcessor class

---
+++ IProc/PixelProcessor/PNGPixelProcessor.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   PNGPixelProcessor.cpp
+ * Author: heshan
+ * 
+ * Created on October 30, 2017, 11:27 PM
+ */
+
+#include "PNGPixelProcessor.h"
+
+PNGPixelProcessor::PNGPixelProcessor() {
+}
+
+PNGPixelProcessor::PNGPixelProcessor(const PNGPixelProcessor& orig) {
+}
+
+PNGPixelProcessor::~PNGPixelProcessor() {
+}
+
---
+++ IProc/PixelProcessor/PNGPixelProcessor.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   PNGPixelProcessor.h
+ * Author: heshan
+ *
+ * Created on October 30, 2017, 11:27 PM
+ */
+
+#ifndef PNGPIXELPROCESSOR_H
+#define PNGPIXELPROCESSOR_H
+
+class PNGPixelProcessor : public PixelProcessor {
+public:
+    PNGPixelProcessor();
+    PNGPixelProcessor(const PNGPixelProcessor& orig);
+    virtual ~PNGPixelProcessor();
+private:
+
+};
+
+#endif /* PNGPIXELPROCESSOR_H */
+
