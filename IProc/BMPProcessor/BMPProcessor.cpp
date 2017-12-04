4737dcae1c1f8b25fbb8f0671b055fc3e5b0595b
Heshan Eranga <erangamx@gmail.com>
2017-12-04 23:44:05 +0530

    changes in BMPProcessor class

--- IProc/BMPProcessor/BMPProcessor.cpp
+++ IProc/BMPProcessor/BMPProcessor.cpp
@@ -87,78 +87,110 @@ int BMPProcessor::readImage(char * filename) {
  */
 int BMPProcessor::writeImage (char * filename, ImageDataStruct imageDataStruct) {
     
-    unsigned char file[14] = {
-        'B','M', // magic
-        0,0,0,0, // size in bytes
-        0,0, // app data
-        0,0, // app data
-        40+14,0,0,0 // start of data offset
-    };
-    unsigned char info[40] = {
-        40,0,0,0, // info hd size
-        0,0,0,0, // width
-        0,0,0,0, // heigth
-        1,0, // number color planes
-        24,0, // bits per pixel
-        0,0,0,0, // compression is none
-        0,0,0,0, // image bits size
-        0x13,0x0B,0,0, // horz resoluition in pixel / m
-        0x13,0x0B,0,0, // vert resolutions (0x03C3 = 96 dpi, 0x0B13 = 72 dpi)
-        0,0,0,0, // #colors in pallete
-        0,0,0,0, // #important colors
-        };
+    unsigned int headers[13];
+    FILE * outfile;
+    int extrabytes;
+    int paddedsize;
+    int x; int y; int n;
+    int red, green, blue;
+    
+    int WIDTH = imageDataStruct.imgWidth;
+    int HEIGHT = imageDataStruct.imgHeight;
+    
+    extrabytes = 4 - ((WIDTH * 3) % 4);                 // How many bytes of padding to add to each
+                                                        // horizontal line - the size of which must
+                                                        // be a multiple of 4 bytes.
+    if (extrabytes == 4)
+       extrabytes = 0;
 
-    int w=imageDataStruct.imgWidth;
-    int h=imageDataStruct.imgHeight;
+    paddedsize = ((WIDTH * 3) + extrabytes) * HEIGHT;
 
-    int padSize  = (4-(w*3)%4)%4;
-    int sizeData = w*h*3 + h*padSize;
-    int sizeAll  = sizeData + sizeof(file) + sizeof(info);
+    // Headers...
+    // Note that the "BM" identifier in bytes 0 and 1 is NOT included in these "headers".
 
-    file[ 2] = (unsigned char)( sizeAll    );
-    file[ 3] = (unsigned char)( sizeAll>> 8);
-    file[ 4] = (unsigned char)( sizeAll>>16);
-    file[ 5] = (unsigned char)( sizeAll>>24);
+    headers[0]  = paddedsize + 54;      // bfSize (whole file size)
+    headers[1]  = 0;                    // bfReserved (both)
+    headers[2]  = 54;                   // bfOffbits
+    headers[3]  = 40;                   // biSize
+    headers[4]  = WIDTH;  // biWidth
+    headers[5]  = HEIGHT; // biHeight
 
-    info[ 4] = (unsigned char)( w   );
-    info[ 5] = (unsigned char)( w>> 8);
-    info[ 6] = (unsigned char)( w>>16);
-    info[ 7] = (unsigned char)( w>>24);
+    // Would have biPlanes and biBitCount in position 6, but they're shorts.
+    // It's easier to write them out separately (see below) than pretend
+    // they're a single int, especially with endian issues...
 
-    info[ 8] = (unsigned char)( h    );
-    info[ 9] = (unsigned char)( h>> 8);
-    info[10] = (unsigned char)( h>>16);
-    info[11] = (unsigned char)( h>>24);
+    headers[7]  = 0;                    // biCompression
+    headers[8]  = paddedsize;           // biSizeImage
+    headers[9]  = 0;                    // biXPelsPerMeter
+    headers[10] = 0;                    // biYPelsPerMeter
+    headers[11] = 0;                    // biClrUsed
+    headers[12] = 0;                    // biClrImportant
 
-    info[20] = (unsigned char)( sizeData    );
-    info[21] = (unsigned char)( sizeData>> 8);
-    info[22] = (unsigned char)( sizeData>>16);
-    info[23] = (unsigned char)( sizeData>>24);
+    outfile = fopen(filename, "wb");
 
-//    stream.write( (char*)file, sizeof(file) );
-//    stream.write( (char*)info, sizeof(info) );
-//
-//    unsigned char pad[3] = {0,0,0};
-//
-//    for ( int y=0; y<h; y++ )
-//    {
-//        for ( int x=0; x<w; x++ )
-//        {
-//            long red = lround( 255.0 * waterfall[x][y] );
-//            if ( red < 0 ) red=0;
-//            if ( red > 255 ) red=255;
-//            long green = red;
-//            long blue = red;
-//
-//            unsigned char pixel[3];
-//            pixel[0] = blue;
-//            pixel[1] = green;
-//            pixel[2] = red;
-//
-//            stream.write( (char*)pixel, 3 );
-//        }
-//        stream.write( (char*)pad, padSize );
-//    }
+    //
+    // Headers begin...
+    // When printing ints and shorts, we write out 1 character at a time to avoid endian issues.
+    //
+
+    fprintf(outfile, "BM");
+
+    for (n = 0; n <= 5; n++)
+    {
+       fprintf(outfile, "%c", headers[n] & 0x000000FF);
+       fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+       fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+       fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+    }
+
+    // These next 4 characters are for the biPlanes and biBitCount fields.
+
+    fprintf(outfile, "%c", 1);
+    fprintf(outfile, "%c", 0);
+    fprintf(outfile, "%c", 24);
+    fprintf(outfile, "%c", 0);
+
+    for (n = 7; n <= 12; n++)
+    {
+       fprintf(outfile, "%c", headers[n] & 0x000000FF);
+       fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+       fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+       fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+    }
+
+    //
+    // Headers done, now write the data...
+    //
+
+    for (y = HEIGHT - 1; y >= 0; y--)     // BMP image format is written from bottom to top...
+    {
+       for (x = 0; x <= WIDTH - 1; x++)
+       {
+
+//          red = reduce(redcount[x][y] + COLOUR_OFFSET) * red_multiplier;
+//          green = reduce(greencount[x][y] + COLOUR_OFFSET) * green_multiplier;
+//          blue = reduce(bluecount[x][y] + COLOUR_OFFSET) * blue_multiplier;
+
+          if (red > 255) red = 255; if (red < 0) red = 0;
+          if (green > 255) green = 255; if (green < 0) green = 0;
+          if (blue > 255) blue = 255; if (blue < 0) blue = 0;
+
+          // Also, it's written in (b,g,r) format...
+
+          fprintf(outfile, "%c", blue);
+          fprintf(outfile, "%c", green);
+          fprintf(outfile, "%c", red);
+       }
+       if (extrabytes)      // See above - BMP lines must be of lengths divisible by 4.
+       {
+          for (n = 1; n <= extrabytes; n++)
+          {
+             fprintf(outfile, "%c", 0);
+          }
+       }
+    }
+
+    fclose(outfile);
     
     return 1;
 }
